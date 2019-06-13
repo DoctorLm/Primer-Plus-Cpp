c6612fc7cd8f2a243a77dbbc2069b7d93516d546
陆敏 <6000099@qq.com>
2019-06-13 18:31:49 +0800

    C++6

---
+++ C++ Primer Plus(第6版)中文版/C++ Primer Plus(第6版)中文版/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Primer Plus(第6版)中文版
+//
+//  Created by 陆敏 on 2019/6/13.
+//  Copyright © 2019 陆敏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
